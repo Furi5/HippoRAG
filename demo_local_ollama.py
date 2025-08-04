260744dafb27555f54e215c750fc0ae353b36caa
fuli <fuli@mr.ai>
2025-08-04 14:22:09 +0800

    add embeding model

--- demo_local_ollama.py
+++ demo_local_ollama.py
@@ -8,8 +8,9 @@ from src.hipporag import HippoRAG
 
 def main():
     df = pd.read_csv("pmid_article.csv")
-    docs = list(df['article'])[:2]
-    
+
+    docs = list(df['article'])[:40]
+
     save_dir = 'output_RAG'  # Define save directory for HippoRAG objects (each LLM/Embedding model combination will create a new subdirectory)
     llm_model_name = 'gemma3:27b'  # Any OpenAI model name
     embedding_model_name = 'nomic-embed-text:latest'  # Embedding model name (NV-Embed, GritLM or Contriever for now)
@@ -29,9 +30,19 @@ def main():
         qa_data = json.load(f)
     for group, qa_dict in qa_data.items():
         for key, qa in qa_dict.items():
-            hipporag.add_qa(
-                queries=f"{qa['question']} {qa['option']}",
+
+            print(f"question: {qa['question']}, options: {qa['options']}")
+            
+            queries_solutions, all_response_message, all_metadata = hipporag.rag_qa(
+                queries=[f"{qa['question']} {qa['options']}"],
                 )
+            for query in queries_solutions:
+                retrieved_docs = query.docs
+                options = query.answer
+                answer = all_response_message
+                print(f"Retrieved documents: {retrieved_docs}")
+                print(f"Options: {options}")
+                print(f"Answer: {answer}")
             break
         break
 
